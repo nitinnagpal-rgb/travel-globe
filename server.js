5e2bc1cfed5ae2f7d3707deadf2fcf43d837febd
Builder <build@example.com>
2026-03-28 23:54:22 +0000

    Integrate admin panel into index.html via hash routing (#admin)

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static("."));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.listen(5000, "0.0.0.0", () => console.log("listening on 5000"));
